4fd2e8cd3e675c5b667435fd6a2a5b4a1eb4b01d
YasoKarim <yasoelshafei@gmail.com>
2023-02-04 22:56:27 +0200

    Documentation Modified

--- Password Generator.py
+++ Password Generator.py
@@ -4,20 +4,22 @@ Password generator
 If it is 10 letters
 60 % => 6 characters 3 (lowercase [a-z])  3 (uppercase [A-Z])
 30 % => 3 digits [0-9]
-10 % => 1 special charcaters [_ | / | \ , ...]
+10 % => 1 special charcaters [ _ | / | \ , etc...]
 Module used
 string 
 ------- 
 ascci_lowercase
 ascci_uppercase
-digts
+digits
 punctutaion -> special characters
-they are lists containing all the constants instead of intilizaing a list
-random -> generates enterd elements in random form
+They are lists containing all the constants instead of intilizaing a list
+
+random -> generates entered elements in random form
 -------
-to import choicing the selected element
-join to join the elements if a list together
-sample takes the list and it's length 
+To import
+choicing => the selected element
+join.element => to concatenate the elements/element/list to the given one
+sample => takes the list and it's length 
 '''
 import string
 import random
